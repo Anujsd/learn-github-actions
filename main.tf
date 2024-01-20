c9a3592460909bd35213cf379f874b70e0120a49
asd234 <anujdube0123@gmail.com>
2024-01-20 23:37:18 +0530

    [terraform] updated workflow

--- main.tf
+++ main.tf
@@ -2,31 +2,31 @@ resource "aws_instance" "myec2" {
   ami           = "ami-0c7217cdde317cfec"
   instance_type = "t2.micro"
   key_name = "github-pair"
-  user_data = <<EOF
+  user_data = <<-EOF
     #!/bin/bash
+    
+    apt-get update
+    apt-get upgrade -y
 
     # Add Docker's official GPG key:
-    apt-get update
-    apt-get install -y ca-certificates curl gnupg
-    install -m 0755 -d /etc/apt/keyrings
+    sudo apt-get install -y ca-certificates curl gnupg
+    sudo install -m 0755 -d /etc/apt/keyrings
     curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
-    chmod a+r /etc/apt/keyrings/docker.gpg
+    sudo chmod a+r /etc/apt/keyrings/docker.gpg
 
     # Add the repository to Apt sources:
-    echo \
-      "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
-      $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
-      tee /etc/apt/sources.list.d/docker.list > /dev/null
-    apt-get update
+    echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+    sudo apt-get update
 
-    apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+    sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
-    groupadd docker
+    sudo groupadd docker
 
-    usermod -aG docker ubuntu
+    sudo usermod -aG docker ubuntu
 
-    reboot
+    sudo reboot
   EOF
+
   tags          = {
     Name = "cicdInstance"
   }
